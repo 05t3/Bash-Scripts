6dcd7e501a2c260f30654ee71d46d8c4072db5da
Oduor Mike <87503468+AnonyMyke@users.noreply.github.com>
2023-05-13 00:48:49 +0300

    Update nmap.sh

--- Homelab-Scripts/nmap.sh
+++ Homelab-Scripts/nmap.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#set e to make the script exit if it encounters an error
+set -e
 
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <ip_address>"
+  exit 1
+fi
+
+ip="$1"
 timestamp=$(date "+%Y.%m.%d")
-ip=$(ifconfig eth0 | grep -w  "inet" | awk '{print $2}')
 directory="/home/ubuntu/report/nmap_scans"
 comprehensive=Full_Port_Scan_For.$timestamp.txt
 final=Services_Running_On.$timestamp.txt
 
-sudo nmap -sCV -T4 -p- $ip --system-dns > $directory/$comprehensive && cat $directory/$comprehensive | egrep "open|filtered|closed" > $directory/$final
+if [[ ! -d "$directory" ]]; then
+  echo "Directory $directory does not exist"
+  exit 1
+fi
+
+sudo nmap -sCV -T4 -p- "$ip" --system-dns > "$directory/$comprehensive" && cat "$directory/$comprehensive" | egrep "open|filtered|closed" > "$directory/$final"
